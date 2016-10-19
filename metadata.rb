c5d408cad871f7773dcf57d002664c01c606d104
Mihir Singh <git.service@mihirsingh.com>
2016-10-19 06:04:16 -0500

    Created a Chef cookbook for MDMA-managed nodes

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'mdma'
+maintainer       'Mihir Singh'
+maintainer_email 'hello@mihirsingh.com'
+license          'Public Domain'
+description      'Configures a MongoDB server'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
